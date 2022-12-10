36c536410ac77b6955ee8d7c7272957d97f76bac
mahanteshuma <mahanteshhonakadabi>
2022-12-10 13:15:08 +0530

    First Commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+
+{  public WebDriver driver;
+    public void setBrowser(){
+
+
+    }
+
+
+}
